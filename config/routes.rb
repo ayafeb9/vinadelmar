e3a62522401f73e205defcfb551a185af96390b7
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-04-18 16:38:03 -0400

    updated navbar, user registration new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'index', to: 'pages#index'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles do
