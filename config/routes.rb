fa74cc19c50460facc4ee189925f1b399a158d36
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-04-20 15:42:36 -0400

    added contacts, modified contacts page to reflect form that was madein _mailer, linked it correctly

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :contacts
   resources :articles do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_225210) do
+ActiveRecord::Schema.define(version: 2020_04_20_185643) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_04_18_225210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
