23e2f2b646670f3d6d08f14e9f0fa4a80e950009
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-05-09 16:08:39 -0400

    after verifying single sender authentication on website, changed from emails to hozuay01@

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'hozuay01@luther.edu'
   layout 'mailer'
 end
--- test.rb
+++ test.rb
@@ -4,13 +4,13 @@ require 'sendgrid-ruby'
 include SendGrid
 
 from = Email.new(email: 'hozuay01@luther.edu')
-to = Email.new(email: 'aya_feb9@hotmail.com')
+to = Email.new(email: 'hozuay01@luther.edu')
 subject = 'Sending with SendGrid is Fun'
 content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
 mail = Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+sg = SendGrid::API.new(api_key: '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')
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
-puts response.headers
+#puts response.headers
