eb490e61277559c463638a4e5147a0c29804b0c8
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-05-11 14:08:17 -0400

    added test.rb to gitignore since it had apikey. replacdd apikey with get from env but stopped working

--- test.rb
+++ test.rb
@@ -9,7 +9,7 @@ subject = 'Sending with SendGrid is Fun'
 content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
 mail = Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: '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')
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
