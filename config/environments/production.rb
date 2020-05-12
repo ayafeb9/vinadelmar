24ed597d5fd96c5757946e070ea3deeb7762a28e
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-05-12 15:11:23 -0400

    changed url of domain, added more info to production.rb, created new smtp.rb with all settings. all in attempt to fix sendgrid error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # added for Udemy course
+ config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://vinadelmar.herokuapp.com/' }
 end
