dbc6c3c17ba28aaf989f81e87032c967b848c80f
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-05-12 14:38:40 -0400

    changed port to 465 from 587

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'yourdomain.com',
   :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :port => 465,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
