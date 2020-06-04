5ca6686ba5178d8d3b2ac602a776dbacc6cc61f9
ayafeb9 <aya.hozumi@kaust.edu.sa>
2020-06-04 14:50:29 -0400

    association between user and posts, also edited post#index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_173651) do
+ActiveRecord::Schema.define(version: 2020_06_04_184401) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_173651) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
